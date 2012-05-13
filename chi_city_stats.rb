f67b549eefb6fddbd2ea66c1020e07484d53a7d8
Tomek Rabczak <tomek.rabczak@gmail.com>
2012-05-13 11:45:37 -0500

    mention caching working

--- chi_city_stats.rb
+++ chi_city_stats.rb
@@ -1,9 +1,9 @@
 require 'rubygems'
 require 'twitter'
+require 'pry'
+require 'set'
 
 load 'auth_module.rb' # GET THIS FROM TOMEK
-
-puts AuthModule::CONSUMER_SECRET
 
 Twitter.configure do |config|
   config.consumer_key       = AuthModule::CONSUMER_KEY
@@ -12,4 +12,18 @@ Twitter.configure do |config|
   config.oauth_token_secret = AuthModule::OAUTH_TOKEN_SECRET
 end
 
-puts Twitter.home_timeline.first.text
+mention_cache = Set.new
+
+loop do
+  mentions = Twitter.mentions
+  mentions.each do |mention|
+    unless mention_cache.include? [mention.text, mention.user.screen_name]
+      puts mention.text
+      puts mention.user.screen_name
+
+      mention_cache << [mention.text, mention.user.screen_name]
+    end
+  end
+  
+  sleep(5)
+end
