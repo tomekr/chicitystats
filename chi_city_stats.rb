5631340afd38cd100f6a0b2eefd17228a4cb5a8e
Tomek Rabczak <tomek.rabczak@gmail.com>
2012-05-13 12:55:01 -0500

    added coordinate parsing

--- chi_city_stats.rb
+++ chi_city_stats.rb
@@ -7,12 +7,12 @@ require 'geokit'
 load 'auth_module.rb' # GET THIS FROM TOMEK
 
 def respond_to_mention(user, text, location)
-  if location
-    crime_rating = get_crime_rating user.location
-    Twitter.update("@#{ user } #{ crime_rating }")
-  else
-    Twitter.update("@#{ user } please enable and attatch location to tweet")
-  end
+  # if location
+  #   crime_count, status = get_crime_rating user.location
+  #   Twitter.update("@#{ user } Thefts: #{ crime_count } Status: #{ status }")
+  # else
+  #   Twitter.update("@#{ user } please enable and attatch location to tweet")
+  # end
 end
 
 def get_crime_rating(rectangular_radius)
@@ -32,8 +32,10 @@ loop do
   mentions = Twitter.mentions
   mentions.each do |mention|
     unless mention_cache.include? [mention.text, mention.user.screen_name]
-      location =  mention.place.bounding_box.coordinates if mention.place
-      respond_to_mention mention.text, mention.user.screen_name, location
+      binding.pry
+      lat, long  =  mention.geo.coordinates if mention.geo
+      coords = {:lat => lat, :lng => long}
+      respond_to_mention mention.text, mention.user.screen_name, coords
       mention_cache << [mention.text, mention.user.screen_name]
     end
   end
