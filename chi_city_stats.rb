a375e27176bcf02f524917606a9e840fccdf957a
Tomek Rabczak <tomek.rabczak@gmail.com>
2012-05-13 11:33:24 -0500

    twitter auth

--- chi_city_stats.rb
+++ chi_city_stats.rb
@@ -1,2 +1,15 @@
 require 'rubygems'
 require 'twitter'
+
+load 'auth_module.rb' # GET THIS FROM TOMEK
+
+puts AuthModule::CONSUMER_SECRET
+
+Twitter.configure do |config|
+  config.consumer_key       = AuthModule::CONSUMER_KEY
+  config.consumer_secret    = AuthModule::CONSUMER_SECRET
+  config.oauth_token        = AuthModule::OAUTH_TOKEN
+  config.oauth_token_secret = AuthModule::OAUTH_TOKEN_SECRET
+end
+
+puts Twitter.home_timeline.first.text
